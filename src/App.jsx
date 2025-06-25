07fc0b9fa7c1824769a60413b224bd0c53d7a9b4
xItzHypeR <gperalta672gmail.com>
2025-06-25 03:25:44 -0600

    Refactor App component structure and update imports for better organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import './styles/index.css'
-import Login from './pages/login.jsx'
-// import Producto from './pages/producto.jsx'
+import React from "react";
+// import Header from "./components/Header";
+import Home from "./components/Home";
+import NewArrivals from "./components/NewArrivals";
+import Shop from "./components/Shop";
+import Brands from "./components/Brands";
+import Trending from "./components/Trending";
+import Subscribe from "./components/Subscribe";
+// import Footer from "./components/Footer";
+import ScrollUp from "./components/ScrollUp";
+
+
+import "/src/styles/variables.css";
+import "/src/styles/styles.css";
+import "/src/styles/swiper.css";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Login /> {/* cambiar esta linea para probar otras paginas */}
-      {/* <Producto /> */}
+      {/* <Header /> */}
+      {/* <Home /> */}
+      <NewArrivals />
+      <Shop />
+      {/* <Brands /> */}
+      {/* <Trending /> */}
+      {/* <Subscribe /> */}
+      {/* <Footer /> */}
+      {/* <ScrollUp /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
